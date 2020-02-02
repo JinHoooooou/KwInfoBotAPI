c7588fd040fb2dd053d344bee5f85f98b0c34690
JinHoooooou <jinho4744@naver.com>
2020-02-02 17:29:56 +0900

    code refactoring for event subscribe

--- slack_kw_info_chatbot/src/main/java/com/slack/chatbot/controller/KwInfoBotController.java
+++ slack_kw_info_chatbot/src/main/java/com/slack/chatbot/controller/KwInfoBotController.java
@@ -28,48 +28,38 @@ import com.slack.chatbot.dto.SendRequsetDTO;
 @RestController
 public class KwInfoBotController {
 	
-	/* Challenge 파라미터 갱신용
-	 * 
-	 * 
-	@RequestMapping(value = "/kwinfobot", method=RequestMethod.POST)
-	public String ChallengeParameterCheck(@RequestBody HashMap<String, Object> requestURL) {
-		System.out.println(requestURL);
-		String challenge_parameter = requestURL.get("challenge").toString();
-		System.out.println(challenge_parameter);
-		return challenge_parameter;
-	}
-	*/
-	
-	
 	@RequestMapping(value = "/kwinfobot", method = RequestMethod.POST)
 	public String ReceiveMessage(@RequestBody RequestBodyDTO request) throws URISyntaxException {
-		Event event = request.getEvent();				//RequestBody의 Event
-		if(event.getType().equals("app_mention")) {
-			String my_text = event.getText();			//Slack channel에 보낸 메세지
-			String split_text[] = my_text.split(" ");	
-			for(String slack_bot_name : split_text) {	//@slackbottest2 추출
-				if(slack_bot_name.equals("<@USJBNQ36D>")) {
-					int start_index = my_text.indexOf(slack_bot_name);
-					String echo_bot_text = my_text.substring(start_index+(slack_bot_name.length())+1);	// @slackbottest2 이후의 text
-					
-					MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
-                    parameters.add("text", echo_bot_text);
-                    parameters.add("channel", event.getChannel());
-                    parameters.add("token", "install_app_token 입력");
-					
-                    System.out.println(parameters);
-                    
-					RestTemplate restTemplate = new RestTemplate();
-					final String baseUrl = "https://slack.com/api/chat.postMessage";
-					URI uri = new URI(baseUrl);
-					
-					ResponseEntity<String> result = restTemplate.postForEntity(uri, parameters, String.class);
-					
-					System.out.println(result);
-					
+		
+		if(request.getChallenge()!=null)
+			return request.getChallenge();
+		else {
+			Event event = request.getEvent();				//RequestBody의 Event
+			if(event.getType().equals("app_mention")) {
+				String my_text = event.getText();			//Slack channel에 보낸 메세지
+				String split_text[] = my_text.split(" ");	
+				for(String slack_bot_name : split_text) {	//@slackbottest2 추출
+					if(slack_bot_name.equals("<@USJBNQ36D>")) {
+						int start_index = my_text.indexOf(slack_bot_name);
+						String echo_bot_text = my_text.substring(start_index+(slack_bot_name.length())+1);	// @slackbottest2 이후의 text
+						
+						MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+	                    parameters.add("text", echo_bot_text);
+	                    parameters.add("channel", event.getChannel());
+	                    parameters.add("token", "install_app_token 입력");
+						
+	                    System.out.println(parameters);
+	                    
+						RestTemplate restTemplate = new RestTemplate();
+						final String baseUrl = "https://slack.com/api/chat.postMessage";
+						URI uri = new URI(baseUrl);
+						
+						ResponseEntity<String> result = restTemplate.postForEntity(uri, parameters, String.class);				
+					}
 				}
 			}
-		}
+		}		
+		
 		return "";
 	}
 	
